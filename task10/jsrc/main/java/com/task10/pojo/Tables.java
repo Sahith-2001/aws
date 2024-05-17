1f7c97c9ba97baabb517062033d2d591642082eb
sahith_reddy <163520570+Sahith-2001@users.noreply.github.com>
2024-05-17 13:26:45 +0530

    Update Tables.java

--- task10/jsrc/main/java/com/task10/pojo/Tables.java
+++ task10/jsrc/main/java/com/task10/pojo/Tables.java
@@ -2,7 +2,7 @@ package com.task10.pojo;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
 
-@DynamoDBTable(tableName = "cmtr-ecf0d511-Tables")
+@DynamoDBTable(tableName = "cmtr-ecf0d511-Tables-test")
 public class Tables {
     @DynamoDBHashKey
     private int id;
